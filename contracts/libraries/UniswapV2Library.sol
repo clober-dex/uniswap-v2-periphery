70920a421d555a8e48f80f7ac3382916a08a7d5d
JChoy <jchoyeclipse@gmail.com>
2024-12-11 00:24:22 +0900

    feat: fix init code hash

--- contracts/libraries/UniswapV2Library.sol
+++ contracts/libraries/UniswapV2Library.sol
@@ -21,7 +21,7 @@ library UniswapV2Library {
                 hex'ff',
                 factory,
                 keccak256(abi.encodePacked(token0, token1)),
-                hex'96e8ac4277198ff8b6f785478aa9a39f403cb768dd02cbee326c3e7da348845f' // init code hash
+                hex'0d77abc719f49a4305cc0409c429738f5f2f1f32808d76576085ca34851e12f1' // init code hash
             ))));
     }
 
